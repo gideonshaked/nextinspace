66d6026f8036394545d526cd5e178c16dde3b5ab
The-Kid-Gid <gideonshaked@gmail.com>
2020-09-12 23:45:01 -0400

    Add space.py
    This is the module for the classes used to store space events.

---
+++ nextinspace/space.py
@@ -0,0 +1,48 @@
+"""Space is the class definitions used to store data from the LL2 API."""
+
+# Basic SpaceEvent parent class
+class SpaceEvent:
+    def __init__(self, mission_name, location, mission_date, mission_description, mission_type):
+        self.mission_name = mission_name
+        self.location = location
+        self.mission_date = mission_date
+        self.mission_description = mission_description
+        self.mission_type = mission_type
+
+
+# Child class adds rocket details
+class LaunchEvent(SpaceEvent):
+    def __init__(
+        self,
+        mission_name,
+        location,
+        mission_date,
+        mission_description,
+        mission_type,
+        rocket_name,
+        payload_leo,
+        payload_gto,
+        liftoff_thrust,
+        liftoff_mass,
+        max_stages,
+        successful_launches,
+        consecutive_successful_launches,
+        failed_launches,
+        maiden_flight_date,
+    ):
+        super().__init__(mission_name, location, mission_date, mission_description, mission_type)
+        self.rocket_name = rocket_name
+        self.payload_leo = payload_leo
+        self.payload_gto = payload_gto
+        self.liftoff_thrust = liftoff_thrust
+        self.liftoff_mass = liftoff_mass
+        self.max_stages = max_stages
+        self.successful_launches = successful_launches
+        self.consecutive_successful_launches = consecutive_successful_launches
+        self.failed_launches = failed_launches
+        self.maiden_flight_date = maiden_flight_date
+
+
+# No additional attributes over SpaceEvent
+class OtherEvent(SpaceEvent):
+    pass
