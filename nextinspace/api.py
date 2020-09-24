71ab1ee8d94a15525537e477033a2e47eec3ec1a
The-Kid-Gid <gideonshaked@gmail.com>
2020-09-23 22:02:19 -0400

    Complete getRocket and getLaunchEvents

--- nextinspace/api.py
+++ nextinspace/api.py
@@ -21,7 +21,64 @@ def getLaunchEvents(num_events=1):
 
     events = []
     for i in range(num_events):
-        pass
+        current = data["results"][i]
+
+        mission_name = current["mission"]["name"]
+        location = current["pad"]["name"] + ", " + current["pad"]["location"]["name"]
+
+        date_string = current["net"]
+        mission_date_unaware = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
+        mission_date = get_localzone().localize(mission_date_unaware)
+
+        mission_description = current["mission"]["description"]
+        mission_type = current["mission"]["type"]
+
+        rocket_url = current["rocket"]["configuration"]["url"]
+        rocket = getRocket(rocket_url)
+
+        events.append(
+            space.LaunchEvent(mission_name, location, mission_date, mission_description, mission_type, rocket)
+        )
+
+    return events
+
+
+def getRocket(url):
+    """Returns Rocket object from API
+
+    Args:
+        url (string): The LL2 API URL of the rocket
+    """
+
+    response = requests.get(url)
+    data = response.json()
+
+    name = data["name"]
+    payload_leo = data["leo_capacity"]
+    payload_gto = data["gto_capacity"]
+    liftoff_thrust = data["to_thrust"]
+    liftoff_mass = data["launch_mass"]
+    max_stages = data["max_stage"]
+    successful_launches = data["successful_launches"]
+    consecutive_successful_launches = data["consecutive_successful_launches"]
+    failed_launches = data["failed_launches"]
+
+    maiden_flight_date_string = data["maiden_flight"]
+    maiden_flight_date_unaware = datetime.strptime(maiden_flight_date_string, "%Y-%m-%d")
+    maiden_flight_date = get_localzone().localize(maiden_flight_date_unaware)
+
+    return space.Rocket(
+        name,
+        payload_leo,
+        payload_gto,
+        liftoff_thrust,
+        liftoff_mass,
+        max_stages,
+        successful_launches,
+        consecutive_successful_launches,
+        failed_launches,
+        maiden_flight_date,
+    )
 
 
 def getOtherEvents(num_events=1):
@@ -36,15 +93,17 @@ def getOtherEvents(num_events=1):
 
     events = []
     for i in range(num_events):
-        mission_name = data["results"][i]["name"]
-        location = data["results"][i]["location"]
+        current = data["results"][i]
 
-        date_string = data["results"][i]["date"]
+        mission_name = current["name"]
+        location = current["location"]
+
+        date_string = current["date"]
         mission_date_unaware = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
         mission_date = get_localzone().localize(mission_date_unaware)
 
-        mission_description = data["results"][i]["description"]
-        mission_type = data["results"][i]["type"]["name"]
+        mission_description = current["description"]
+        mission_type = current["type"]["name"]
 
         events.append(space.OtherEvent(mission_name, location, mission_date, mission_description, mission_type))
 
