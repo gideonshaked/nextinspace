5a522cc9dbe54f65ac557b40042adea3d982c13a
The-Kid-Gid <gideonshaked@gmail.com>
2020-10-06 20:11:22 -0400

    Add python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
         "Natural Language :: English",
