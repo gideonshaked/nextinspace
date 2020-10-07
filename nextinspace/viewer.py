e55f8c8d37af84b3fa7b2c597620581a7abf1898
Gideon Shaked <gideonshaked@gmail.com>
2020-10-06 21:27:53 -0400

    Windows support color output

--- nextinspace/viewer.py
+++ nextinspace/viewer.py
@@ -1,14 +1,18 @@
 """Viewer for terminal output"""
 
+from colorama import deinit, init
+
 from nextinspace import space
 
 
 def display_list(items_list, verbosity):
     show_top()
+    init()  # For compatibility with Windows
     items_list[0].display(verbosity)
     for s in items_list[1:]:
         show_divider()
         s.display(verbosity)
+    deinit()
     show_bottom()
 
 
