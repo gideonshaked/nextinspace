dd55662a7805852a7c2a8688916f7a9c6d05a11b
The-Kid-Gid <gideonshaked@gmail.com>
2020-12-28 23:36:48 -0500

    Make python -m nextinspace work for CLI
    Replace driver code in __main__.py with an invocation of console.run()

--- nextinspace/__main__.py
+++ nextinspace/__main__.py
@@ -1,25 +1,3 @@
-from nextinspace import api, parser, space, viewer
+from nextinspace.cli import console
 
-
-def main():
-    args = parser.get_args()
-
-    if args.verbose:
-        VERBOSITY = space.Verbosity.VERBOSE
-    elif args.quiet:
-        VERBOSITY = space.Verbosity.QUIET
-    else:
-        VERBOSITY = space.Verbosity.NORMAL
-
-    if args.events_only:
-        items = api.get_events(args.num_items)
-    elif args.launches_only:
-        items = api.get_launches(args.num_items, VERBOSITY)
-    else:
-        items = api.get_all(args.num_items, VERBOSITY)
-
-    viewer.display_list(items, VERBOSITY)
-
-
-if __name__ == "__main__":
-    main()
+console.run()  # type: ignore[no-untyped-call]
